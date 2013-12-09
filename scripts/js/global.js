3973685a43156405e641bb3059dc2c5b8b3a7dde
Skylar Challand <skylar@oakmade.com>
2013-12-09 13:15:30 -0500

    prevent JS errors on pages without .post-listing

--- scripts/js/global.js
+++ scripts/js/global.js
@@ -3,12 +3,14 @@
 
   container = document.querySelector(".post-listing");
 
-  imagesLoaded(container, function() {
-    var pckry;
-    return pckry = new Packery(container, {
-      itemSelector: ".post-preview",
-      gutter: 0
+  if (container) {
+    imagesLoaded(container, function() {
+      var pckry;
+      return pckry = new Packery(container, {
+        itemSelector: ".post-preview",
+        gutter: 0
+      });
     });
-  });
+  }
 
 }).call(this);
